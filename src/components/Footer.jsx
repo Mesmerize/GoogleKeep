84a4d6aef8b915ac0e603a9d2070d8ab21675c3c
Brandon <67185805+Mesmerize@users.noreply.github.com>
2021-01-14 23:42:18 -0800

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    var date = new Date();
+    var year = date.getFullYear();
+
+   return (
+   <footer>
+    <p>Copyright ⓒ {year}</p>
+   </footer>
+        );
+}
+
+export default Footer;
